20e773a7a5355b23b89030ae5b102029312a9bf7
Parth Panchal <parthpanchal53@gmail.com>
2021-05-15 08:25:20 +0530

    Added dotenv for hiding API keys

--- src/backend.js
+++ src/backend.js
@@ -1,13 +1,16 @@
 import firebase from "firebase";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcq_ehL1S1ybwZ_KPSs_2r_mb3VRUvSUA",
-  authDomain: "lofi-chat.firebaseapp.com",
-  projectId: "lofi-chat",
-  storageBucket: "lofi-chat.appspot.com",
-  messagingSenderId: "877788630630",
-  appId: "1:877788630630:web:11c720c92827449ebbe7a7",
-  measurementId: "G-FRL5E54HR8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
