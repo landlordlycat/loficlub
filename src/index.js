52200d634bce543865c6cb2020dc58d1e5d61bd3
neeraj15022001 <gneeraj32595@gmail.com>
2021-06-25 15:00:12 +0530

    added PWA Install Popup

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "./styles/App.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "jquery/dist/jquery.min.js"
+// Install Popup
+import InstallPopup from "./InstallAppPopup"
 
 ReactDOM.render(
   <React.StrictMode>
+    <InstallPopup />
     <Routes />
   </React.StrictMode>,
   document.getElementById("root")
